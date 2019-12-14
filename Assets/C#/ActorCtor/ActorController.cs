6561481f289d3e4c51fa95738c079b7c5484c760
MyGrindelWald <leatherlove001@gmail.com>
2019-12-14 13:43:15 +0800

    no message

---
+++ Assets/C#/ActorCtor/ActorController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActorController : MonoBehaviour
+{
+    #region AnimatorID
+    private int forwardID = Animator.StringToHash("forward");
+
+    #endregion
+
+
+
+    public GameObject Model;
+    public PlayerInput Pi;
+    [SerializeField]
+    private Animator anim;
+    private Rigidbody rigid;
+
+
+    #region Vector3
+    private Vector3 moveingVec;
+    #endregion
+    #region Parameter
+    public float WalkSpeed;
+    #endregion
+
+    public bool isOpen = false;
+    void Awake()
+    {
+        Pi = GetComponent<PlayerInput>();
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat(forwardID, Pi.Dmag);
+        if (Model && Pi.Dmag > 0.1f)
+        {
+            Vector3 targetForward = Vector3.Slerp(Model.transform.forward, Pi.Dvec, 0.3f);
+            Model.transform.forward = targetForward;
+
+        }
+        moveingVec = Pi.Dmag * Model.transform.forward;
+    }
+    private void FixedUpdate()
+    {
+        if (rigid)
+        {
+            //rigid.position += moveingVec * Time.fixedDeltaTime*WalkSpeed;
+            rigid.velocity = WalkSpeed*  moveingVec+new Vector3(0,rigid.velocity.y,0);
+        }
+    }
+   
+}
---
+++ Assets/C#/Input/PlayerInput.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    //Viriable
+    
+    public string KeyUp ="w";
+    public string KeyDown = "s";
+    public string KeyLeft = "a";
+    public string KeyRight = "d";
+
+    public float Dup;
+    public float Dright;
+    public float Dmag;
+    public Vector3 Dvec;
+
+    private float targetDup;
+    private float targetDright;
+
+    public float Time;
+    public float VelocityDup;
+    public float VelocityDright;
+
+
+    public bool InputEnabled = true;
+    void Awake()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        targetDup = (Input.GetKey(KeyUp) ? 1 : 0) - (Input.GetKey(KeyDown) ? 1 : 0);
+        targetDright = (Input.GetKey(KeyRight) ? 1 : 0) - (Input.GetKey(KeyLeft) ? 1 : 0);
+
+
+#endif
+
+#if UNITY_IPHONE
+
+#endif
+
+        if (!InputEnabled)
+        {
+            targetDup = 0;
+            targetDright = 0;
+        }
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref VelocityDup, Time);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref VelocityDright, Time);
+        Vector2 tempDaxis = squareToCircle(new Vector2( Dright,Dup));
+        
+
+        Dmag = Mathf.Sqrt(tempDaxis.x * tempDaxis.x + tempDaxis.y * tempDaxis.y);
+        Dvec = Dright * transform.right + Dup * transform.forward;
+    }
+    Vector2 squareToCircle(Vector2 input)
+    {
+        Vector2 output;
+        output.x = input.x * Mathf.Sqrt(1 - (input.y * input.y) / 2);
+        output.y = input.y * Mathf.Sqrt(1 - (input.x * input.x) / 2);
+
+        return output;
+
+    }
+
+}
+
+
